9594eca0a45c3d54adb211000673c8e409ccfe77
monthly-library <147822364+monthly-library@users.noreply.github.com>
2023-10-28 21:00:04 +0900

    q180 옵션 수정

--- SAP-C02-KR_V14.95/question_180.js
+++ SAP-C02-KR_V14.95/question_180.js
@@ -1,4 +1,52 @@
-const question_180=`QUESTION NO: 180 소매 회사는 비즈니스 파트너인 다른 회사에 일련의 데이터 파일을 제공해야 합니다. 이 파일은 소매 회사에 속한 계정 A의 Amazon S3 버킷에 저장됩니다. 비즈니스 파트너 회사는 오전 1시 사용자 중 한 명을 원합니다. User_DataProcessor. 자신의 AWS 계정(계정 B)에서 파일에 액세스합니다. User_DataProcessor가 S3 버킷에 성공적으로 액세스할 수 있도록 회사에서 수행해야 하는 단계 조합은 무엇입니까? (2개를 선택하세요.) A. 계정에서 S3 버킷에 대한 교차 출처 리소스 공유(CORS) 기능을 켭니다. B. 계정 A에서 S3 버킷 정책을 다음과 같이 설정합니다. C. 계정 A에서 S3 버킷 정책을 다음과 같이 설정합니다. D. 계정 B에서 User_DataProcessor의 권한을 다음과 같이 설정합니다. E. 계정 Bt에서 User_DataProcessor의 권한을 다음과 같이 설정합니다. Answer: C,D Explanation: https://aws.amazon.com/premiumsupport/knowledge-center/cross-account-access-s3/
+const question_180=`QUESTION NO: 180 소매 회사는 비즈니스 파트너인 다른 회사에 일련의 데이터 파일을 제공해야 합니다. 이 파일은 소매 회사에 속한 계정 A의 Amazon S3 버킷에 저장됩니다. 비즈니스 파트너 회사는 오전 1시 사용자 중 한 명을 원합니다. User_DataProcessor. 자신의 AWS 계정(계정 B)에서 파일에 액세스합니다. User_DataProcessor가 S3 버킷에 성공적으로 액세스할 수 있도록 회사에서 수행해야 하는 단계 조합은 무엇입니까? (2개를 선택하세요.) 
+A. 계정에서 S3 버킷에 대한 교차 출처 리소스 공유(CORS) 기능을 켭니다. 
+B. 계정 A에서 S3 버킷 정책을 다음과 같이 설정합니다. 
+{
+    "Effect": "Allow",
+    "Action": [
+        "s3:GetOjbect",
+        "s3:ListBucket""
+    ],
+    "Resource": "arn:aws:s3:::AccountABucketName/*"
+}
+C. 계정 A에서 S3 버킷 정책을 다음과 같이 설정합니다. 
+{
+    "Effect": "Allow",
+    "Pricipal": {
+        "AWS": "arn:aws:iam::AccountB:user/User_DataProcessr"
+    ],
+    "Action": [
+        "s3:GetObject",
+        "s3:ListBuecket"
+    ],
+    "Resource": [
+        "arn:aws:s3:::AccountABucketName/*"
+    ]
+}
+D. 계정 B에서 User_DataProcessor의 권한을 다음과 같이 설정합니다. 
+{
+    "Effect": "Allow",
+    "Action": [
+        "s3:GetObject",
+        "s3:ListBuecket"
+    ],
+    "Resource": "arn:aws:s3:::AccountABucketName/*"
+}
+E. 계정 B에서 User_DataProcessor의 권한을 다음과 같이 설정합니다. 
+{
+    "Effect": "Allow",
+    "Pricipal": {
+        "AWS": "arn:aws:iam::AccountB:user/User_DataProcessr"
+    ],
+    "Action": [
+        "s3:GetObject",
+        "s3:ListBuecket"
+    ],
+    "Resource": [
+        "arn:aws:s3:::AccountABucketName/*"
+    ]
+}
+Answer: C,D Explanation: https://aws.amazon.com/premiumsupport/knowledge-center/cross-account-access-s3/
 GPT 3.5 says "텍스트에서 제공된 답변은 잘못되었습니다.
 
 Account B의 User_DataProcessor가 Account A가 소유한 S3 버킷의 데이터 파일에 액세스할 수 있도록 하려면 다음 단계를 수행해야 합니다:
